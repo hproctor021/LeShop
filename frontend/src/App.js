6cdcef7bc83c20310359823b1291a1f365743b6e
hproctor021 <hproctor021@gmail.com>
2021-02-20 18:17:19 -0600

    updated theme, and OrderScreen bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
 
 
 
@@ -62,6 +63,11 @@ const App = () => {
           <Route 
             path='/placeorder'
             component={PlaceOrderScreen}
+          />
+
+          <Route 
+            path='/order/:id'
+            component={OrderScreen}
           />
 
           <Route 
