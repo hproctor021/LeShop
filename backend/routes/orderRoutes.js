3117e312eea11ac941a7d1f26b9cc7756e698a58
hproctor021 <hproctor021@gmail.com>
2021-03-01 14:44:29 -0600

    built backend controller and route for logged in user orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router()
 import { 
     addOrderItems,
     getOrderById,
-    updateOrderToPaid
+    updateOrderToPaid,
+    getMyOrders
 } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 
 router.route('/').post(protect, addOrderItems)
+router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 // always put '/:id' route below the others so it doesn't get 
 // confused by trying to use what follows the '/' as an :id
