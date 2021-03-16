f271590d19bc88f2ee31442c808574711f6d4372
hproctor021 <hproctor021@gmail.com>
2021-03-16 12:28:13 -0500

    cleared localstorage of previous user/ users order info

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Form, Button, Row, Col } from 'react-bootstrap'
+import { Form, Button, Row, Col, Table } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
@@ -123,7 +124,34 @@ const ProfileScreen = ({ location, history }) => {
                             </tr>
                         </thead>
                         <tbody>
-                            {orders.map(orders)}
+                            {orders.map(order => (
+                                <tr key={order._id}>
+                                    <td>{order._id}</td>
+                                    <td>{order.createdAt.substring(0, 10)}</td>
+                                    <td>{order.totalPrice}</td>
+                                    <td>
+                                        {order.isPaid ? (
+                                            order.paidAt.substring(0, 10)
+                                        ) : (
+                                            <i className='fas fa-times' style={{color: 'red'}}></i>
+                                        )}
+                                     </td>
+                                     <td>
+                                        {order.isDelivered ? (
+                                            order.deliveredAt.substring(0, 10)
+                                        ) : (
+                                            <i className='fas fa-times' style={{color: 'red'}}></i>
+                                        )}
+                                     </td>
+                                     <td>
+                                         <LinkContainer id={`/order/${order._id}`}>
+                                             <Button className='btn-sm' variant='light'>
+                                                 Details
+                                            </Button>
+                                         </LinkContainer>
+                                     </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </Table>
                 )}
